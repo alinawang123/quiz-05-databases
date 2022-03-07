80c1b054272147d3793cf6bab1534aefc5989b5a
AlinaWX <72416330+alinawang123@users.noreply.github.com>
2022-03-07 00:31:08 -0800

    Update index.js

--- index.js
+++ index.js
@@ -24,10 +24,56 @@ app.get("/todos", async (req, res) => {
   res.json(posts);
 });
 
+
 // creates a todo item 
+app.post("/todos", async (req, res) => {
+ const {title, authorEmail} = req.body;
+ const todoItem = await prisma.todoItem.create({
+   data: {
+     title,
+     author:{ connect: { email: authorEmail } },
+   },
+  });
+  res.json(todoItem);
+});
+
 // deletes a todo item by id
+app.delete("/todos/:id", async (req, res) =>{
+  const deleteTodoItem = await prisma.todoItem.delete({
+    where: {
+      id: req.params.id,
+    }
+  });
+});
+
 // get a todo item by id
+app.get("/todos/:id", async (req, res) =>{
+  const todoItem = await prisma.todoItem.findUnique({
+    where: {
+      id: req.params.id,
+    }
+  });
+  if(todoItem) {
+    res.status(200).json(todoItem);
+  } else{
+    res.status(404).send(`todo item id: ${req.params.id} not found`);
+  }
+});
+
 // updates a todo item by id
+app.put("/todos/:id", async (req, res) =>{
+  const body = req.body;
+  const todoItem = await prisma.todoItem.update({
+    where: {
+      id: req.params.id,
+    },
+    data: {
+      title : body.title,
+      author:{ connect: { email: body.authorEmail } },
+    },
+  });
+  res.json(todoItem);
+});
 
 app.listen(8000, () => {
   console.log("Server running on http://localhost:8000 🎉 🚀");
